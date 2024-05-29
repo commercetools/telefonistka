90de8432f1beb118538e805ea0cc1c2a6d274855
Hannes Gustafsson <hannes.gustafsson@commercetools.com>
2024-05-29 14:32:56 +0100

    mocks: use go run to reduce dependency
    Using go run will automatically install the tool and run it if it is not
    installed already.

--- internal/pkg/mocks/mocks.go
+++ internal/pkg/mocks/mocks.go
@@ -4,4 +4,4 @@ package mocks
 
 //  mockgen -package=mocks -destination authenticator.go k8s.io/apiserver/pkg/authentication/authenticator Token
 //  mockgen -source=../../../vendor/github.com/argoproj/argo-cd/v2/pkg/apiclient/application/application.pb.go -destination=mock_argocd_application.go -package=mocks
-//go:generate mockgen -destination=argocd_application.go -package=mocks github.com/argoproj/argo-cd/v2/pkg/apiclient/application ApplicationServiceClient
+//go:generate go run github.com/golang/mock/mockgen@v1.6.0 -destination=argocd_application.go -package=mocks github.com/argoproj/argo-cd/v2/pkg/apiclient/application ApplicationServiceClient
