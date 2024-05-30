a9ab8296a8ff2fddf1a80ea7bb5f7e4985c74579
Oded Ben Ozer <oded.benozer@commercetools.com>
2024-05-30 10:53:07 +0200

    Cleanup and try to use make file config

--- internal/pkg/mocks/mocks.go
+++ internal/pkg/mocks/mocks.go
@@ -2,6 +2,4 @@ package mocks
 
 // This package contains generated mocks
 
-//  mockgen -package=mocks -destination authenticator.go k8s.io/apiserver/pkg/authentication/authenticator Token
-//  mockgen -source=../../../vendor/github.com/argoproj/argo-cd/v2/pkg/apiclient/application/application.pb.go -destination=mock_argocd_application.go -package=mocks
 //go:generate go run github.com/golang/mock/mockgen@v1.6.0 -destination=argocd_application.go -package=mocks github.com/argoproj/argo-cd/v2/pkg/apiclient/application ApplicationServiceClient
