82225a36796fcc700fe906133791cd186452337c
Oded Ben Ozer <oded.benozer@commercetools.com>
2024-06-28 16:52:51 +0200

    Use the correct field in the event payload

--- internal/pkg/githubapi/github.go
+++ internal/pkg/githubapi/github.go
@@ -385,7 +385,7 @@ func handleCommentPrEvent(ghPrClientDetails GhPrClientDetails, ce *github.IssueC
 	_, _ = ghPrClientDetails.GetSHA()
 
 	checkboxPattern := `(?m)^\s*-\s*\[(.)\]\s*<!-- telefonistka-argocd-branch-sync -->.*$`
-	checkboxWaschecked, checkboxIsChecked := analyzeCommentUpdateCheckBox(*ce.Comment.Body, *ce.Issue.Body, checkboxPattern)
+	checkboxWaschecked, checkboxIsChecked := analyzeCommentUpdateCheckBox(*ce.Comment.Body, *ce.Changes.Body.From, checkboxPattern)
 	if !checkboxWaschecked && checkboxIsChecked {
 		ghPrClientDetails.PrLogger.Infof("Sync Checkbox was checked")
 		if config.AllowSyncArgoCDAppfromBranchPathRegex != "" {
