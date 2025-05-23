fde1abe2972676974030ae5bf6c827be3575a12a
Hannes Gustafsson <hannes.gustafsson@commercetools.com>
2025-05-23 04:52:57 +0100

    Add another test for debugging

--- telefonistka_test.go
+++ telefonistka_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 	"time"
 
+	"helm.sh/helm/v3/pkg/chartutil"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -94,6 +95,114 @@ func TestTelefonistka(t *testing.T) {
 		case *corev1.Pod:
 			portForward(t, cluster.Config, argoNamespace, o.Name, argoForwardAddr, argoForwardPorts)
 		}
+	})
+
+	// Template a demo application, setting it to track the created repository.
+	var data struct{ RepoURL string }
+	data.RepoURL = repository.GetHTMLURL()
+	templated := readTemplate(t, "additional.yaml", data)
+	applyResource(t, cluster.Config, argoNamespace, templated)
+
+	// At this point we have an initial connection to use, so let's get the
+	// default password, and login so we get a token we can use.
+	adminPassword := getDecodedSecret(t, clientset.CoreV1(), argoNamespace, argoInitialPasswordSecretName, argoInitialPasswordSecretKey)
+	token := newArgoToken(t, conn, argoUsername, adminPassword.String())
+	t.Logf("You can log into Argo CD on %q using %q and %q as the password", "https://"+argoServerAddr, argoUsername, adminPassword)
+
+	startTelefonistka(t, token, argoServerAddr, webhookSecret)
+
+	// There is no good way to wait for execution and start of server; TODO: as
+	// mentioned above, refactor entrypoint such that it will be just as easy
+	// to spin up an isolated instance in code
+	time.Sleep(5 * time.Second)
+
+	wsURL := createRepoHook(t, gh, repository, webhookSecret)
+
+	// dst, src
+	forwardData(t, ctx, forwardTarget, wsURL)
+
+	//  Setup initial state of repository based on testdata.
+	first := createCommit(t, gh, repository, "heads/main", "Initial", os.DirFS(path.Join("testdata", t.Name(), "start")))
+	updateRef(t, gh, repository, "heads/main", first.GetSHA())
+
+	// Create a PR with some changes based on testdata.
+	branch := createBranch(t, gh, repository, first, "upgrade")
+	createCommit(t, gh, repository, "heads/upgrade", "Upgrade application", os.DirFS(path.Join("testdata", t.Name(), "pr")))
+
+	var n TestPR
+	n.Title = "Upgrade to vX.X.X"
+	n.Ref = branch.GetRef()
+	n.Base = "main"
+	n.Body = "This upgrades to the bleeding edge."
+
+	createPR(t, gh, repository, &n)
+}
+
+func TestHelm(t *testing.T) {
+	// Make test interactive by waiting for explicit interrupt before
+	// finishing. This allows setting things up so that caller can interact
+	// with resources before they're all cleaned up.
+	ctx, cancel := contextWithGracePeriod(t.Context(), 10*time.Second)
+	defer cancel()
+
+	ctx, cancel = signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+	defer waitFor(ctx)
+
+	var (
+		argoNamespace                 = "argocd"
+		argoLocalPort                 = "8083"
+		argoContainerPort             = "8080"
+		argoServer                    = "argocd-server"
+		argoForwardPorts              = []string{strings.Join([]string{argoLocalPort, argoContainerPort}, ":")}
+		argoForwardAddr               = []string{"127.0.0.1"}
+		argoServerAddr                = net.JoinHostPort("localhost", argoLocalPort)
+		argoUsername                  = "admin"
+		argoInitialPasswordSecretName = "argocd-initial-admin-secret" //nolint:gosec // not a password
+		argoInitialPasswordSecretKey  = "password"
+	)
+
+	var (
+		// TODO make sure this aligns with what is configured when starting the
+		// server. For now it is hardcoded.
+		forwardTarget = "http://localhost:8080/webhook"
+		webhookSecret = rand.Text()
+	)
+
+	cluster := newCluster(t)
+	clientset := newClientset(t, cluster.Config)
+	gh := newGithubClient(t)
+	repository := createRepository(t, gh)
+	conn := newArgoGRPCConnection(t, cluster.Config, argoServerAddr)
+
+	createNamespace(t, clientset.CoreV1().Namespaces(), argoNamespace)
+
+	// Alternatively install without Helm.
+	//
+	// installYamlFile := getFile(t, "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml")
+	// applyResource(t, cl.Config, argoNamespace, installYamlFile)
+
+	loadLocalImage(t, newDockerClient(t), cluster.Provider, "gcr.io/ct-services/argo-cd-helmfile:v0.5.0") // sha256:ed34582d67cab4fbba9057134858043b64852ba8ace0cc7b45ac995f8d47337c
+
+	vals := readValuesFile(t, "argo.values.yaml")
+	releaseExternalChart(t, cluster.TemporaryConfigFile, argoNamespace, "https://argoproj.github.io/argo-helm", "argo-cd", vals)
+
+	waitForReady(t, clientset.CoreV1().RESTClient(), argoNamespace, "Pod", "app.kubernetes.io/name="+argoServer, "", func(o any) {
+		switch o := o.(type) {
+		case *corev1.Pod:
+			portForward(t, cluster.Config, argoNamespace, o.Name, argoForwardAddr, argoForwardPorts)
+		}
+	})
+
+	createNamespace(t, clientset.CoreV1().Namespaces(), "cert-manager")
+	releaseExternalChart(t, cluster.TemporaryConfigFile, "cert-manager", "https://charts.jetstack.io", "cert-manager", chartutil.Values{
+		"installCRDs":                    "true",
+		"crds.enabled":                   "true",
+		"prometheus.enabled":             "false",
+		"startupapicheck.enabled":        "false",
+		"global.crds.enabled":            "true",
+		"global.prometheus.enabled":      "false",
+		"global.startupapicheck.enabled": "false",
 	})
 
 	// Template a demo application, setting it to track the created repository.
