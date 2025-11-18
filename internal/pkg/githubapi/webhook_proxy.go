368aa4d54e79ae815a5ece47b210cf48d90aac49
Hannes Gustafsson <hannes.gustafsson@commercetools.com>
2025-11-18 14:31:05 +0100

    Simplify proxy logic
    Remove some unnecessary stuff and prefer waitgroup

--- internal/pkg/githubapi/webhook_proxy.go
+++ internal/pkg/githubapi/webhook_proxy.go
@@ -4,12 +4,12 @@ import (
 	"bytes"
 	"context"
 	"crypto/tls"
-	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 	"regexp"
 	"strings"
+	"sync"
 
 	"github.com/commercetools/telefonistka/internal/pkg/configuration"
 	prom "github.com/commercetools/telefonistka/internal/pkg/prometheus"
@@ -34,7 +34,7 @@ func generateListOfEndpoints(listOfChangedFiles []string, config *configuration.
 	return maps.Keys(endpoints)
 }
 
-func proxyRequest(ctx context.Context, skipTLSVerify bool, originalHttpRequest *http.Request, body []byte, endpoint string, responses chan<- string) {
+func proxyRequest(ctx context.Context, skipTLSVerify bool, originalHttpRequest *http.Request, body []byte, endpoint string) {
 	tr := &http.Transport{}
 	if skipTLSVerify {
 		tr = &http.Transport{
@@ -45,7 +45,6 @@ func proxyRequest(ctx context.Context, skipTLSVerify bool, originalHttpRequest *
 	req, err := http.NewRequestWithContext(ctx, originalHttpRequest.Method, endpoint, bytes.NewBuffer(body))
 	if err != nil {
 		slog.Error("Error creating request to endpoint", "endpoint", endpoint, "err", err)
-		responses <- fmt.Sprintf("Failed to create request to %s", endpoint)
 		return
 	}
 	req.Header = originalHttpRequest.Header.Clone()
@@ -54,28 +53,17 @@ func proxyRequest(ctx context.Context, skipTLSVerify bool, originalHttpRequest *
 	resp, err := client.Do(req)
 	if err != nil {
 		slog.Error("Error proxying request to endpoint", "endpoint", endpoint, "err", err)
-		responses <- fmt.Sprintf("Failed to proxy request to %s", endpoint)
 		return
-	} else {
-		slog.Debug("Webhook successfully forwarded to endpoint", "endpoint", endpoint)
 	}
+	slog.Debug("Webhook successfully forwarded to endpoint", "endpoint", endpoint)
 	defer resp.Body.Close()
 
 	_ = prom.InstrumentProxyUpstreamRequest(resp)
 
-	respBody, err := io.ReadAll(resp.Body)
-
 	if !strings.HasPrefix(resp.Status, "2") {
-		slog.Error("Got non 2XX HTTP status from endpoint", "endpoint", endpoint, "status", resp.Status, "body", body)
+		body, _ := io.ReadAll(resp.Body)
+		slog.Error("Got non 2XX HTTP status from endpoint", "endpoint", endpoint, "status", resp.Status, "body", string(body))
 	}
-
-	if err != nil {
-		slog.Error("Error reading response body from endpoint", "endpoint", endpoint, "err", err)
-		responses <- fmt.Sprintf("Failed to read response from %s", endpoint)
-		return
-	}
-
-	responses <- string(respBody)
 }
 
 func handleProxyForward(ctx context.Context, config *configuration.Config, listOfChangedFiles []string, httpRequest *http.Request, payload []byte) {
@@ -86,24 +74,13 @@ func handleProxyForward(ctx context.Context, config *configuration.Config, listO
 	// Speeding up ArgoCD reconcile loops
 	endpoints := generateListOfEndpoints(listOfChangedFiles, config)
 
-	// Create a channel to receive responses from the goroutines
-	responses := make(chan string)
-
-	// Use a buffered channel with the same size as the number of endpoints
-	// to prevent goroutines from blocking in case of slow endpoints
-	results := make(chan string, len(endpoints))
-
-	// Start a goroutine for each endpoint
+	var wg sync.WaitGroup
 	for _, endpoint := range endpoints {
-		go proxyRequest(ctx, config.WhProxtSkipTLSVerifyUpstream, httpRequest, payload, endpoint, responses)
+		wg.Add(1)
+		go func(endpoint string) {
+			defer wg.Done()
+			proxyRequest(ctx, config.WhProxtSkipTLSVerifyUpstream, httpRequest, payload, endpoint)
+		}(endpoint)
 	}
-
-	// Wait for all goroutines to finish and collect the responses
-	for i := 0; i < len(endpoints); i++ {
-		result := <-responses
-		results <- result
-	}
-
-	close(responses)
-	close(results)
+	wg.Wait()
 }
