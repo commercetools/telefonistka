187c41c387e5df1adddb6dba0d7fa61bc7b6df3e
Hannes Gustafsson <hannes.gustafsson@commercetools.com>
2026-02-21 17:41:04 +0000

    argocd,githubapi,webhook: silence slog output in tests
    Add TestMain to each package that discards the default slog
    handler. Without this, tests that exercise production code
    emit timestamped log lines to stderr, cluttering go test -v
    output.

---
+++ webhook/testmain_test.go
@@ -0,0 +1,15 @@
+package webhook
+
+import (
+	"io"
+	"log/slog"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// Silence default slog output during tests.  Individual tests
+	// that need logging can call slog.SetDefault with their own handler.
+	slog.SetDefault(slog.New(slog.NewTextHandler(io.Discard, nil)))
+	os.Exit(m.Run())
+}
